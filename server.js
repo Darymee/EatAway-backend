d7126782e25bfa853bbc358c6c42dc489b764b5f
Darymee <danieledaniele3005@gmail.com>
2023-10-14 14:02:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,18 +5,24 @@ const app = require("./app");
 
 mongoose.set("strictQuery", false);
 
-const { HOST_URI } = process.env;
+const { HOST_URI, PORT } = process.env;
 
 (async function () {
   try {
-    await mongoose.connect(HOST_URI);
+    const connection = await mongoose.connect(HOST_URI);
     console.log("Database connection successful");
-
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
-    });
+    return connection;
   } catch (error) {
     console.log("Error connecting to database", error.message);
+
     process.exit(1);
   }
-})();
+})()
+  .then(() => {
+    app.listen(PORT || 4444, function () {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) =>
+    console.log(`Server not running. Error message: ${err.message}`)
+  );
